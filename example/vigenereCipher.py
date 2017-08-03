70c4e5a24f9668e0b22afa18a4341c33796e0e30
meprogrammerguy <b65535@gmail.com>
2017-08-03 10:59:12 -0500

    moving stuff around

---
+++ example/vigenereCipher.py
@@ -0,0 +1,69 @@
+# Vigenere Cipher (Polyalphabetic Substitution Cipher)
+# http://inventwithpython.com/hacking (BSD Licensed)
+
+import pyperclip
+
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+def main():
+    # This text can be copy/pasted from http://invpy.com/vigenereCipher.py
+    myMessage = """Adiz Avtzqeci Tmzubb wsa m Pmilqev halpqavtakuoi, lgouqdaf, kdmktsvmztsl, izr xoexghzr kkusitaaf. Vz wsa twbhdg ubalmmzhdad qz hce vmhsgohuqbo ox kaakulmd gxiwvos, krgdurdny i rcmmstugvtawz ca tzm ocicwxfg jf "stscmilpy" oid "uwydptsbuci" wabt hce Lcdwig eiovdnw. Bgfdny qe kddwtk qjnkqpsmev ba pz tzm roohwz at xoexghzr kkusicw izr vrlqrwxist uboedtuuznum. Pimifo Icmlv Emf DI, Lcdwig owdyzd xwd hce Ywhsmnemzh Xovm mby Cqxtsm Supacg (GUKE) oo Bdmfqclwg Bomk, Tzuhvif'a ocyetzqofifo ositjm. Rcm a lqys ce oie vzav wr Vpt 8, lpq gzclqab mekxabnittq tjr Ymdavn fihog cjgbhvnstkgds. Zm psqikmp o iuejqf jf lmoviiicqg aoj jdsvkavs Uzreiz qdpzmdg, dnutgrdny bts helpar jf lpq pjmtm, mb zlwkffjmwktoiiuix avczqzs ohsb ocplv nuby swbfwigk naf ohw Mzwbms umqcifm. Mtoej bts raj pq kjrcmp oo tzm Zooigvmz Khqauqvl Dincmalwdm, rhwzq vz cjmmhzd gvq ca tzm rwmsl lqgdgfa rcm a kbafzd-hzaumae kaakulmd, hce SKQ. Wi 1948 Tmzubb jgqzsy Msf Zsrmsv'e Qjmhcfwig Dincmalwdm vt Eizqcekbqf Pnadqfnilg, ivzrw pq onsaafsy if bts yenmxckmwvf ca tzm Yoiczmehzr uwydptwze oid tmoohe avfsmekbqr dn eifvzmsbuqvl tqazjgq. Pq kmolm m dvpwz ab ohw ktshiuix pvsaa at hojxtcbefmewn, afl bfzdakfsy okkuzgalqzu xhwuuqvl jmmqoigve gpcz ie hce Tmxcpsgd-Lvvbgbubnkq zqoxtawz, kciup isme xqdgo otaqfqev qz hce 1960k. Bgfdny'a tchokmjivlabk fzsmtfsy if i ofdmavmz krgaqqptawz wi 1952, wzmz vjmgaqlpad iohn wwzq goidt uzgeyix wi tzm Gbdtwl Wwigvwy. Vz aukqdoev bdsvtemzh rilp rshadm tcmmgvqg (xhwuuqvl uiehmalqab) vs sv mzoejvmhdvw ba dmikwz. Hpravs rdev qz 1954, xpsl whsm tow iszkk jqtjrw pug 42id tqdhcdsg, rfjm ugmbddw xawnofqzu. Vn avcizsl lqhzreqzsy tzif vds vmmhc wsa eidcalq; vds ewfvzr svp gjmw wfvzrk jqzdenmp vds vmmhc wsa mqxivmzhvl. Gv 10 Esktwunsm 2009, fgtxcrifo mb Dnlmdbzt uiydviyv, Nfdtaat Dmiem Ywiikbqf Bojlab Wrgez avdw iz cafakuog pmjxwx ahwxcby gv nscadn at ohw Jdwoikp scqejvysit xwd "hce sxboglavs kvy zm ion tjmmhzd." Sa at Haq 2012 i bfdvsbq azmtmd'g widt ion bwnafz tzm Tcpsw wr Zjrva ivdcz eaigd yzmbo Tmzubb a kbmhptgzk dvrvwz wa efiohzd."""
+    myKey = 'ASIMOV'
+    myMode = 'decrypt' # set to 'encrypt' or 'decrypt'
+
+    if myMode == 'encrypt':
+        translated = encryptMessage(myKey, myMessage)
+    elif myMode == 'decrypt':
+        translated = decryptMessage(myKey, myMessage)
+
+    print('%sed message:' % (myMode.title()))
+    print(translated)
+    pyperclip.copy(translated)
+    print()
+    print('The message has been copied to the clipboard.')
+
+
+def encryptMessage(key, message):
+    return translateMessage(key, message, 'encrypt')
+
+
+def decryptMessage(key, message):
+    return translateMessage(key, message, 'decrypt')
+
+
+def translateMessage(key, message, mode):
+    translated = [] # stores the encrypted/decrypted message string
+
+    keyIndex = 0
+    key = key.upper()
+
+    for symbol in message: # loop through each character in message
+        num = LETTERS.find(symbol.upper())
+        if num != -1: # -1 means symbol.upper() was not found in LETTERS
+            if mode == 'encrypt':
+                num += LETTERS.find(key[keyIndex]) # add if encrypting
+            elif mode == 'decrypt':
+                num -= LETTERS.find(key[keyIndex]) # subtract if decrypting
+
+            num %= len(LETTERS) # handle the potential wrap-around
+
+            # add the encrypted/decrypted symbol to the end of translated.
+            if symbol.isupper():
+                translated.append(LETTERS[num])
+            elif symbol.islower():
+                translated.append(LETTERS[num].lower())
+
+            keyIndex += 1 # move to the next letter in the key
+            if keyIndex == len(key):
+                keyIndex = 0
+        else:
+            # The symbol was not in LETTERS, so add it to translated as is.
+            translated.append(symbol)
+
+    return ''.join(translated)
+
+
+# If vigenereCipher.py is run (instead of imported as a module) call
+# the main() function.
+if __name__ == '__main__':
+    main()
