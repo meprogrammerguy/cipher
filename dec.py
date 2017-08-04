ae57845873bcafc20411a8c5a29c594610a10681
meprogrammerguy <b65535@gmail.com>
2017-08-04 14:00:29 -0500

    encode and decode tools

---
+++ dec.py
@@ -0,0 +1,27 @@
+import pyperclip
+import vigenereCipher
+import sys, getopt
+
+def main(argv):
+  password = ''
+  try:
+    opts, args = getopt.getopt(argv,"hp:",["pword="])
+  except getopt.GetoptError:
+    print ('dec.py -p <password>')
+    sys.exit(2)
+  for opt, arg in opts:
+    if opt == '-h':
+      print ('dec.py -p <password>')
+      sys.exit()
+    elif opt in ("-p", "--pword"):
+      password = arg
+  if password != '':
+    myMessage = ''
+    translated = ''
+    myMessage = pyperclip.paste()
+    translated = vigenereCipher.decryptMessage(password, myMessage)
+    pyperclip.copy(translated)
+    print('done.')
+    
+if __name__ == "__main__":
+  main(sys.argv[1:])
---
+++ enc.py
@@ -0,0 +1,27 @@
+import pyperclip
+import vigenereCipher
+import sys, getopt
+
+def main(argv):
+  password = ''
+  try:
+    opts, args = getopt.getopt(argv,"hp:",["pword="])
+  except getopt.GetoptError:
+    print ('enc.py -p <password>')
+    sys.exit(2)
+  for opt, arg in opts:
+    if opt == '-h':
+      print ('enc.py -p <password>')
+      sys.exit()
+    elif opt in ("-p", "--pword"):
+      password = arg
+  if password != '':
+    myMessage = ''
+    translated = ''
+    myMessage = pyperclip.paste()
+    translated = vigenereCipher.encryptMessage(password, myMessage)
+    pyperclip.copy(translated)
+    print('done.')
+    
+if __name__ == "__main__":
+  main(sys.argv[1:])
